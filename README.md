66ccfd65cda5bb492f95ea199857aa85b02efae9
Lennart Hase <lenny.glk@gmail.com>
2014-04-17 17:46:34 +0100

    Uploaded

---
+++ README.md
@@ -0,0 +1,126 @@
+<div class="header"></div>
+<div class="title">
+  <div class="hex_ani">
+    
+  </div>
+  <div class="text">jQuery Plugin: Hex</div>
+</div>
+<div class="body">
+  <div class="wrapper">
+    <span class="title">Getting Started</span>
+    <p>
+      jQuery is required for the plugin to work:<br><br>
+      <code>http://code.jquery.com/jquery-latest.min.js</code>
+      <br><br>
+      The element in which the hex effect is ment to be run needs to have the <code>position: relative</code> or <code>position: absolute</code> set in the css.
+      <br><br>
+      The plugin can be added to your file like usual:<br><br>
+      <code>&lt;script type="text/javascript" src="lib/hex.js"&gt;&lt;/script&gt;</code>
+    </p>
+    <span class="title">Basic Usage</span>
+    <p>
+      To use the effect with its derfault settings:
+      <br><br>
+      <code>$(".element").hex();</code>
+      <br><br>
+      This will reveal the element with the animation running from right to left and using hexagons with the default background <code>#eee</code>
+    </p>
+    <span class="title">Options</span>
+    <p>
+      All of the available options are listed below:
+      <br>
+      </p><ul>
+        <li>show</li>
+        <li>direction</li>
+        <li>background</li>
+        <li>duration</li>
+        <li>delay</li>
+      </ul>
+    <p></p>
+    <span class="title">&nbsp; &nbsp; &nbsp;Option: Show</span>
+    <p class="sub">
+      Values:
+      <br><br>
+      <code>default: true </code><br><br><code>accepted: BOOL</code>
+      <br><br>
+      The show option allows you to specifiy if you want to show or hide the element. The following code shows how to hide an element:
+      <br><br>
+      <code>$(".element").hex({show:false});</code>
+      <br><br>
+      and to show it:
+      <br><br>
+      <code>$(".element").hex({show:true});</code>
+    </p>
+    <span class="title">&nbsp; &nbsp; &nbsp;Option: Direction</span>
+    <p class="sub">
+      Values:
+      <br><br>
+      <code>default: "right" </code><br><br><code>accepted: "right", "left", "up", "down", "random", "center"</code>
+      <br><br>
+      The direction option allows you to specify in which direction the hexagons will come in or go out. For example:
+      <br><br>
+      <code>$(".element").hex({direction: "random"});</code>
+      <br><br>
+      This will remove/add the hexagons randomly until the whole object is covered or revealed completely.
+    </p>
+    <span class="title">&nbsp; &nbsp; &nbsp;Option: Background</span>
+      <p class="sub">
+        Values:
+        <br><br>
+        <code>default: "#eee" </code><br><br><code>accepted: "css color"</code>
+        <br><br>
+        The background option allows you to change the color of the hexagons, the default color is <code>#eee</code>. For example:
+        <br><br>
+        <code>$(".element").hex({background: "#005da6"});</code>
+        <br><br>
+        This will result in the hexagons being this color:<br><br>
+        <div class="hex"></div>
+      <br>
+    </p>
+    <span class="title">&nbsp; &nbsp; &nbsp;Option: Duration</span>
+    <p class="sub">
+      Values:
+      <br><br>
+      <code>default: 10 </code><br><br><code>accepted: int &gt; 0</code>
+      <br><br>
+      The duration option allows you to set how long it takes for hexagons to start vanishing. This value is randomly generated with respect to the location of the hexagon. By changing the duraiton value will be multiply the delay. So decreasing the duration value will cause the delay to be less than the hexagons will vanish earlier. For example
+      <br><br>
+      <code>$(".element").hex({duration: 5});</code>
+      <br><br>
+      This will result in the hexagons vanishing in half the time.
+    </p>
+    <span class="title">&nbsp; &nbsp; &nbsp;Option: Delay</span>
+    <p class="sub">
+      Values:
+      <br><br>
+      <code>default: 0 </code><br><br><code>accepted: int &gt; -1</code>
+      <br><br>
+      The amount of time in ms until the animation will run. For example
+      <br><br>
+      <code>$(".element").hex({delay: 1000});</code>
+      <br><br>
+      This will result that the animation will start 1s after the code above is executed.
+    </p>
+    <span class="title">Example</span>
+    <p>
+      This is the animation used on loading this doc.
+      <br><br>
+      <code>$(document).ready(function(){<br> 
+            &nbsp;&nbsp;&nbsp;&nbsp;show_hex();<br>
+            &nbsp;});<br>
+            <br><br>
+            &nbsp;function show_hex(){<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;$(".hex_ani").hex({show: true, direction: "left", background: "#0769ad", duration: 5, delay: 5000});
+            &nbsp;&nbsp;&nbsp;&nbsp;setTimeout("hide_hex()",10000);
+            &nbsp;}
+            <br><br>
+            &nbsp;function hide_hex(){<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;$(".hex_ani").hex({show: false, direction: "left", background: "#0769ad", duration: 5, delay: 5000});
+            &nbsp;&nbsp;&nbsp;&nbsp;setTimeout("show_hex()",10000);
+            &nbsp;}</code>
+    </p>
+  </div>
+</div>
+<div class="footer">
+  
+</div>
---
+++ hex.js
@@ -0,0 +1,116 @@
+(function ( $ ) {		  
+    $.fn.hex = function( options ) {
+ 
+        // This is the easiest way to have default options.
+        var settings = $.extend({
+            // These are the defaults.
+          show: true,
+          direction: "right",
+          background: "#eee",
+          duration: 10,
+          delay: 100
+        }, options );
+      
+        var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+        var uid = randLetter + Date.now();
+        
+        if(!settings.show){
+          $("html head").append("<style type='text/css'>.hex"+uid+":before {content: ''; width: 0; height: 0; border-bottom: 12px solid "+settings.background+"; border-left: 22px solid transparent; border-right: 22px solid transparent; position: absolute; top: -12px;} .hex"+uid+" {z-index: inherit; display: none; opacity: 0; margin-top: 12px; width: 44px; height: 25px; background-color: "+settings.background+"; position: absolute; top: 300px; margin-left: 1px;} .hex"+uid+":after { content: ''; width: 0; position: absolute; bottom: -12px; border-top: 12px solid "+settings.background+"; border-left: 22px solid transparent; border-right: 22px solid transparent;}</style>");
+        }
+        else{
+          $("html head").append("<style type='text/css'>.hex"+uid+":before {content: ''; width: 0; height: 0; border-bottom: 12px solid "+settings.background+"; border-left: 22px solid transparent; border-right: 22px solid transparent; position: absolute; top: -12px;} .hex"+uid+" {z-index: inherit; margin-top: 12px; width: 44px; height: 25px; background-color: "+settings.background+"; position: absolute; top: 300px; margin-left: 1px;} .hex"+uid+":after { content: ''; width: 0; position: absolute; bottom: -12px; border-top: 12px solid "+settings.background+"; border-left: 22px solid transparent; border-right: 22px solid transparent;}</style>");
+        }
+      
+        $(this).each(function(index){
+          var w = $(this).width();
+          console.log("w:"+w);
+          var h = $(this).height();
+          console.log("h:"+h);
+          var a = Math.ceil((w/40))*Math.ceil((h/25));
+          //var x = $(this).offset().left-30;
+          var x = -30;
+          console.log("x:"+x);
+          //var y = $(this).offset().top-30;
+          var y = -30;
+          console.log("y:"+y);
+          var r = 0;
+          var r_y= 0;
+          for(var i=0;i<a;i++){
+            if(r_y>Math.ceil(w/40)){
+              r++;
+              r_y = 0;
+            }
+            if(r%2 == 0){
+              $(this).append("<div class='hex"+uid+"' style='margin-left: 23px; top:"+(y+(36*r))+"px; left:"+(x+(44*r_y))+"px;'></div>");
+            }
+            else{
+              $(this).append("<div class='hex"+uid+"' style='top:"+(y+(36*r))+"px; left:"+(x+(44*r_y))+"px;'></div>");
+            }
+            console.log("y_val: "+(y+(37*r)));
+
+            r_y++;
+          }
+        });
+      
+        $(this).each(function(index){
+           $(".hex"+uid,this).each(function(){
+             if(settings.direction == "right"){
+               var d = parseInt($(this).css("left"));
+             }
+             else if(settings.direction == "left"){
+               var d = parseInt($(this).parent().width()) - parseInt($(this).css("left"));
+             }
+             else if(settings.direction == "up"){
+               var d = $(this).parent().height() - $(this).offset().top;
+             }
+             else if(settings.direction == "down"){
+               var d = $(this).offset().top;
+             }
+             else if(settings.direction == "random"){
+               var d = Math.random()*200;
+             }
+             else if(settings.direction == "center"){
+               var w = parseInt($(this).parent().width())/2;
+               var l = parseInt($(this).css("left"));
+               var h = parseInt($(this).parent().height())/2;
+               var top = parseInt($(this).css("top"));
+               if(l>w){
+                 if(top>h){
+                   var d = parseInt(l) - parseInt(w) + top;
+                 }
+                 else{
+                   var d = parseInt(l) - parseInt(w) - top;
+                 }
+               }else{
+                 if(top>h){
+                   var d = parseInt(w) - parseInt(l) - top;
+                 }
+                 else{
+                   var d = parseInt(w) - parseInt(l) - top;
+                 }
+               }
+             }
+             if(settings.duration <= 0){
+               settings.duration = 1;
+             }     
+             if(settings.delay < 0){
+               settings.delay = 1;
+             }
+             var t = settings.delay + (d*settings.duration*Math.random());
+             if(!settings.show){
+               $(this).css("display","block");
+               $(this).delay(t).animate({
+                 opacity: 1
+               }, 600);
+             }
+             else{
+               $(this).delay(t).animate({
+                 opacity: 0
+               }, 600, function(){$(this).css("display","none");});
+             }
+           });
+        });
+ 
+    };
+ 
+}( jQuery ));
